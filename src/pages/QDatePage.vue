e08b60819f2d1eaf97cdf6afaa18d7daacd5dc7b
miralda <miraldalexandra15@gmail.com>
2021-11-24 21:12:52 +0100

    create component and page

---
+++ src/pages/QDatePage.vue
@@ -0,0 +1,34 @@
+<template>
+    <!-- <div class="q-pa-md">
+    <div class="q-pb-sm">
+      Model: {{ days }}
+    </div>
+
+    <q-date v-model="days" multiple />
+  </div> -->
+  <qdate></qdate>
+</template>
+
+<script>
+import { ref } from 'vue'
+// 1
+// import QDate from 'components/QDate.vue'
+
+export default {
+  components: {
+    // 1
+    // 'qdate': QDate
+    // 2
+    'qdate': require('components/QDate.vue').default
+  },
+    setup () {
+        return {
+        days: ref([ '2019/02/01', '2019/02/10' ])
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
